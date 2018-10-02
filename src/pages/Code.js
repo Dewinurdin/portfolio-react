ee709af88ac811d4ce4e633cf36dcf1daec88f4e
dewinurdin <dewinurdin@gmail.com>
2018-10-02 10:24:38 -0500

    fixed error snowflakes

--- src/pages/Code.js
+++ src/pages/Code.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Grid, Row, Col, Thumbnail, Button, Image } from 'react-bootstrap';
+import { Grid, Row, Col, Image } from 'react-bootstrap';
 import "../components/stylings/ColorScheme";
 
 class Code extends Component {
